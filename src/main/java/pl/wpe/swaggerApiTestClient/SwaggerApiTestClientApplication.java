a7d1f46d1e63fba78e01d9625c1f58e39c5673b5
qq <w@p>
2018-06-24 22:07:36 +0200

    projekt po generacji initializerem

---
+++ src/main/java/pl/wpe/swaggerApiTestClient/SwaggerApiTestClientApplication.java
@@ -0,0 +1,12 @@
+package pl.wpe.swaggerApiTestClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerApiTestClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerApiTestClientApplication.class, args);
+	}
+}
